e59837665366df899dc6f54e5383438a77a0a5d5
john <johnmarc@axiomalaska.com>
2018-04-24 16:25:53 -0800

    added the static dir for hunts

--- public/scripts/api/HuntsAPI.js
+++ public/scripts/api/HuntsAPI.js
@@ -1,11 +1,11 @@
 //ExampleAPI
 module.exports = {
 	filters: function() {
-		return Mustache.render("/{{{rootUrl}}}/api/hunts/filters", {rootUrl:rootUrl});
+		return Mustache.render("{{{rootUrl}}}/api/hunts/filters", {rootUrl:rootUrl});
 	},
 
 	pointData: function(lat, lon){
-		return Mustache.render("/{{{rootUrl}}}/api/hunts/pointData", {rootUrl:rootUrl})
+		return Mustache.render("{{{rootUrl}}}/api/hunts/pointData", {rootUrl:rootUrl})
 	}
 	
 }
